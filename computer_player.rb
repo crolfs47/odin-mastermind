ba52d166c39a89ca255fffa1bd4cdf2f257217a7
Cathy Rolfs <crolfs47@gmail.com>
2022-11-11 13:49:40 -0600

    Add method to generate all possible codes

--- computer_player.rb
+++ computer_player.rb
@@ -2,7 +2,7 @@
 
 class ComputerPlayer
   def initialize
-    
+    @possible_codes = []
   end
 
   def guess_code
@@ -13,5 +13,11 @@ class ComputerPlayer
   def create_code
     @code = Array.new(4) { rand(1...6) }.join.split('')
     @code
+  end
+
+  def generate_all_codes
+    numbers = [1, 2, 3, 4, 5, 6]
+    @possible_codes = numbers.permutation(4).to_a
+    @possible_codes
   end
 end
