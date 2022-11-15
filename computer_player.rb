0fa221118248579491ee19c56a128091493992da
Cathy Rolfs <crolfs47@gmail.com>
2022-11-15 00:39:59 -0600

    The winner is the COMPUTER - ALEX IS COMPUTER BEEP BOOP

--- computer_player.rb
+++ computer_player.rb
@@ -1,11 +1,9 @@
 # class for the computer player methods and variables
 
-require_relative 'game_board'
-
 class ComputerPlayer
   def initialize
     @possible_codes = generate_all_codes
-    @game_board = GameBoard.new
+    @all_codes = generate_all_codes
   end
 
   def create_code
@@ -17,28 +15,48 @@ class ComputerPlayer
     # random code generator, started w/ this, prob can delete
     # @guess = Array.new(4) { rand(1...6) }.join.split('')
     puts "Guess #{guess_count}: please input four digits (1-6)"
-    if guess_count == 1
-      @guess = ['1', '1', '2', '2']
-    else
-      p @possible_codes
-      @guess = @possible_codes[0]
-    end
-             
+    @possible_codes.delete(@guess)
+    @guess = if guess_count == 1
+               %w[1 1 2 2]
+             else
+               @possible_codes[0]
+             end
   end
 
   def eliminate_codes_correct_position(exact_matches, number_matches)
-    @possible_codes.each_with_index do |possible_code, i|
-      exact = @game_board.correct_positions(possible_code.clone, @guess.clone)
-      number = @game_board.correct_numbers(possible_code.clone, @guess.clone)
-      @possible_codes.delete_at(i) if exact_matches <= exact && number_matches <= number
+    temp_possible_codes = []
+    @possible_codes.clone.each_with_index do |all_code, _i|
+      @possible_codes.clone.each_with_index do |compare_code, _j|
+        exact = correct_positions(all_code, compare_code)
+        number = correct_numbers(all_code, compare_code)
+        temp_possible_codes.push(compare_code) if exact == exact_matches && number >= number_matches
+      end
     end
+    @possible_codes = temp_possible_codes.uniq
+  end
+
+  def correct_positions(code, guess)
+    correct_position = 0
+    guess.each_index do |i|
+      correct_position += 1 if guess[i] == code[i]
+    end
+    correct_position
+  end
+
+  def correct_numbers(code, guess)
+    correct_number = 0
+    guess.each_index do |i|
+      correct_number += 1 if code.include?(guess[i])
+    end
+    correct_number
   end
 
   def eliminate_codes_correct_number; end
 
   def generate_all_codes
-    numbers = ['1', '2', '3', '4', '5', '6']
-    @possible_codes = numbers.permutation(4).to_a
+    numbers = %w[1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 5 6
+                 6 6 6]
+    @possible_codes = numbers.permutation(4).to_a.uniq
     @possible_codes
   end
 end
--- game.rb
+++ game.rb
@@ -2,13 +2,11 @@
 
 require_relative 'human_player'
 require_relative 'computer_player'
-require_relative 'game_board'
 
 class Game
   def initialize
     @guess_count = 1
     @game_over = false
-    @game_board = GameBoard.new
     puts 'Welcome to Mastermind!'
   end
 
@@ -68,16 +66,38 @@ class Game
   def evaluate_guess(code, guess)
     temp_code = code.clone
     temp_guess = guess.clone
-    @exact_matches = @game_board.correct_positions(temp_code, temp_guess)
-    @number_matches = @game_board.correct_numbers(temp_code, temp_guess)
+    @exact_matches = correct_positions(temp_code, temp_guess)
+    @number_matches = correct_numbers(temp_code, temp_guess)
     give_hint(@exact_matches, @number_matches)
-    @player1.eliminate_codes_correct_position(@exact_matches,@number_matches)
-    
+    @player1.eliminate_codes_correct_position(@exact_matches, @number_matches)
 
     # # Stores hint as an array, not sure if I'll need this
     # @hint = ['X'] * @exact_match + ['O'] * @number_match
   end
 
+  def correct_positions(code, guess)
+    correct_position = 0
+    guess.each_index do |i|
+      next unless guess[i] == code[i]
+
+      correct_position += 1
+      code[i] = '*'
+      guess[i] = '*'
+    end
+    correct_position
+  end
+
+  def correct_numbers(code, guess)
+    correct_number = 0
+    guess.each_index do |i|
+      next unless guess[i] != '*' && code.include?(guess[i])
+
+      correct_number += 1
+      code[code.find_index(guess[i])] = 'x'
+      guess[i] = 'x'
+    end
+    correct_number
+  end
 
   
   def give_hint(exact_matches, number_matches)
